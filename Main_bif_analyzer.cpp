b1423affc9bc4077db158f3a638bf2da507af208
stefano3012line <andreanesi1212@gmail.com>
2026-03-25 09:02:48 +0100

    aggiunta una cartella contenente i bif da analizzare

--- Main_bif_analyzer.cpp
+++ Main_bif_analyzer.cpp
@@ -4,7 +4,7 @@ int main(){
     std::string filename;
     std::cout<<"==>"<<"inserire il file da analizzare"<<std::endl<<"==>";
     std::cin>>filename;
-    std::vector<variable> O_var = marginalizer(node_sort(read_var((filename))));    
+    std::vector<variable> O_var = marginalizer(node_sort(read_var(("file_bif/"+filename+".bif"))));    
     return 0;
 }
 //aggiungere runtime
